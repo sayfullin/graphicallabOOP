f57e6555d4b7d1e2596902a8d1ec60ec9f03fd6f
Culminate <bum033@gmail.com>
2017-12-17 18:32:48 +0500

    add: lineWidth to polygon function

--- drawfunctions.js
+++ drawfunctions.js
@@ -1,3 +1,28 @@
+'use strict';
+
+class spolygon {
+    constructor() {
+        this.ctx = initcanvas('OOP');
+        this.left = 100;
+        this.top = 100;
+        this.height = 100;
+        this.width = 100;
+        this.angle = 0;
+        this.sides = 4;
+        this.strokestyle = 'black';
+        this.linewidth = 1;
+        this.fillstyle = 'gray';
+    }
+    draw() {
+        polygon(this.ctx, this.left, this.top, this.height, this.width, this.angle, this.sides, this.strokestyle, this.linewidth, this.fillstyle);
+    }
+    move(left, top) {
+        this.left = left;
+        this.top = top;
+        draw();
+    }
+}
+
 function initcanvas(id) {
     let canvas = document.getElementById(id);
     let context = canvas.getContext('2d');
@@ -7,17 +32,17 @@ function initcanvas(id) {
     return context;
 }
 
-function polygon(ctx, left, top, radius, angle, sides, strokestyle, fillstyle) {
+function polygon(ctx, left, top, height, width, angle, sides, strokestyle, linewidth, fillstyle) {
     if (sides < 3) {
         return;
     }
     ctx.translate(left,top);
     ctx.rotate((Math.PI / 180) * angle);
     ctx.beginPath();
-    ctx.moveTo(radius, 0);
+    ctx.moveTo(width, 0);
     let a = ((Math.PI * 2)/sides);
     for (let i = 1; i < sides; i++) {
-        ctx.lineTo(radius * Math.cos(a * i), radius * Math.sin(a * i));
+        ctx.lineTo(width * Math.cos(a * i), height * Math.sin(a * i));
     }
     ctx.closePath();
     if (fillstyle != 'none') {
@@ -25,6 +50,7 @@ function polygon(ctx, left, top, radius, angle, sides, strokestyle, fillstyle) {
         ctx.fill();
     }
     if (strokestyle != 'none') {
+        ctx.lineWidth = linewidth;
         ctx.strokeStyle = strokestyle;
         ctx.stroke();
     }
@@ -32,5 +58,5 @@ function polygon(ctx, left, top, radius, angle, sides, strokestyle, fillstyle) {
     ctx.translate(-left,-top);
 }
 
-let context = initcanvas('OOP');
-polygon(context, 100, 100, 50, 0, 4, 'blue', 'red');
+var newpoly = new spolygon;
+newpoly.draw();
