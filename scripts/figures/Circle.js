51cf317b4d58dbc4f1c02e67275fff1624c3a490
Culminate <bum033@gmail.com>
2017-12-30 01:05:56 +0500

    add: ellipse draw function

--- scripts/figures/Circle.js
+++ scripts/figures/Circle.js
@@ -4,6 +4,21 @@ class Circle extends Figure{
   }
 
   draw(){
-    console.log('drawing Circle');
+    let radiusX = this.width / 2;
+    let radiusY = this.height / 2;
+    let rotation = (Math.PI / 180) * this.angle;
+    this.ctx.beginPath();
+    this.ctx.ellipse(this.x, this.y, radiusX, radiusY, rotation, 0, Math.PI * 2, true);
+    this.ctx.closePath();
+
+    if (this.color != 'none') {
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+    if (this.borderColor != 'none') {
+        this.ctx.lineWidth = this.borderWidth;
+        this.ctx.strokeStyle = this.borderColor;
+        this.ctx.stroke();
+    }
   }
 }
