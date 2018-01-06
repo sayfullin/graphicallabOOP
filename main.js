228886d4aedf6164bed47004254dcadbeb8daf01
Ruslan Sayfullin <uraltus@yandex.ru>
2018-01-06 13:33:08 +0500

    merge template into master

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 $( document ).ready(function(){
+  $('select[name="colorpicker_color"]').simplecolorpicker({ picker: true });
+  $('select[name="colorpicker_color"]').simplecolorpicker('selectColor', '#7bd148');
+  $('select[name="colorpicker_bordercolor"]').simplecolorpicker({ picker: true });
+  $('select[name="colorpicker_bordercolor"]').simplecolorpicker('selectColor', '#ffb878');
+
+
   var canvas = document.getElementById('canvas');
   var ctx =  canvas.getContext('2d');
+
+  canvas.width = canvas.parentElement.clientWidth
+  canvas.height = canvas.parentElement.clientHeight;
 
   poligon = new Polygon(ctx, 120, 180, 50, 50, 15, 2, 'grey', 'black', 4);
   poligon.draw();
