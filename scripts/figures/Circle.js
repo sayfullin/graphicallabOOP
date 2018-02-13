e90ed902815893a0b89786ce35ea0174f99c0166
Culminate <bum033@gmail.com>
2018-02-13 21:55:28 +0500

    add: circle draw function

--- scripts/figures/Circle.js
+++ scripts/figures/Circle.js
@@ -9,6 +9,21 @@ class Circle extends Figure{
 
   draw(ctx){
     if (!ctx) ctx = this.ctx;
-    console.log('drawing Circle');
+    ctx.translate(this.x, this.y);
+    ctx.rotate((Math.PI / 180) * this.angle);
+    ctx.beginPath();
+    ctx.ellipse(0, 0, this.width, this.height, 0, 0, 2 * Math.PI);
+    ctx.closePath();
+    if (this.color != undefined) {
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+    if (this.borderColor != undefined && this.borderWidth){
+        ctx.lineWidth = this.borderWidth;
+        ctx.strokeStyle = this.borderColor;
+        ctx.stroke();
+    }
+    ctx.rotate(-(Math.PI / 180) * this.angle);
+    ctx.translate(-this.x, -this.y);
   }
 }
